97fba121de0031fa92111cb42007a94c2bcfbf95
wlgud0402 <wlgudrlgus@naver.com>
2020-11-03 18:43:35 +0900

    difference with blank and None

---
+++ python/blank_none.py
@@ -0,0 +1,27 @@
+def Blank():
+    a = ""
+    if a:
+        print("Not Blank")
+    else:
+        print("Blank")
+
+    if a is None:
+        print("None")
+    else:
+        print("Not None")
+
+
+Blank()
+
+
+def None():
+    b = None
+    if b:
+        print("Not Blank")
+    else:
+        print("Blank")
+
+    if b is None:
+        print("None")
+    else:
+        print("Not None")
