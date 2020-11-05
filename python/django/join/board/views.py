9603cbf16c4f525aa89f562a4d2fbec088f33e72
wlgud0402 <wlgudrlgus@naver.com>
2020-11-06 01:16:18 +0900

    raw sql

--- python/django/join/board/views.py
+++ python/django/join/board/views.py
@@ -18,13 +18,27 @@ def boards_by_user(request):
 
 def board(request):
     # 내부적으로 시행하지만 join과는 원칙적으로 다르다. 많은 select문의 실행
-    print("여기ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ")
+    user = User.objects.get(id=1)
+    print("유저: ", user)
+
+    user_board = Board.objects.filter(user_id=user)
+    print("찾은 유저의 글들 => filter 사용", user_board)
+
     boards_with_no_related = Board.objects.all()
-    print(boards_with_no_related)
+    print("글전체 Board.objects.all", boards_with_no_related)
+
+    join_user_board = Board.objects.select_related('user')
+    print("select_related를 사용한 유저의 글들: ", join_user_board)
+
+    prefetch_board_to_user = Board.objects.prefetch_related('user')
+    print("prefetch_board_to_user : =>>>>", prefetch_board_to_user)
 
     boards = Board.objects \
         .select_related('user') \
         # .filter(user=user)
     return render(request, 'board/list.html', {
         "boards": boards,
-    }, {"boards_with_no_related": boards_with_no_related})
+        "boards_with_no_related": boards_with_no_related
+    },)
+    # <!-- {% for board_with_no related in boards_with_no_related %}
+    # {{board_with_no related}} {% endfor %} -->
